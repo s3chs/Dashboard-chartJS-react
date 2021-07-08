00a7069b1eb54b6aa9f98d9e238a6ef71a2eb77e
sechs <philippe.buteau@skema.edu>
2021-07-08 11:56:11 +0200

    navigation done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./App.css";
+import DashboardEmployees from "./Pages/DashboardEmployees/DashboardEmployees";
+import DashboardFinance from "./Pages/DashboardFinance/DashboardFinance";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Sidebar />
+      <Switch>
+        <Route path="/" exact component={DashboardFinance}></Route>
+        <Route
+          path="/dashboardEmployees"
+          exact
+          component={DashboardEmployees}
+        ></Route>
+      </Switch>
+    </Router>
   );
 }
