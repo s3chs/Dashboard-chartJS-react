01eecf892b066f7e5e4b7d5135ecb23be4087b64
sechs <philippe.buteau@skema.edu>
2021-07-08 12:49:12 +0200

    barchart done and displayed

--- src/Pages/DashboardFinance/DashboardFinance.js
+++ src/Pages/DashboardFinance/DashboardFinance.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./DashboardFinance.css";
+import BarChart from "../../Components/AllChart/BarChart";
 
 export default function DashboardFinance() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="global-container">
+      <h1>Les résultats de l'année : 2020</h1>
+
+      <form>
+        <label htmlFor="year">Choisissez une date</label>
+        <select id="year">
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+        </select>
+      </form>
+      <div className="dashboard-container">
+        <BarChart />
+      </div>
+    </div>
+  );
 }
